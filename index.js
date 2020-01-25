3ff42f43cb852dfc8a03095d69ebcbfb0d0d2f58
vytautassugintas <vytautas.sugintas@gmail.com>
2020-01-26 01:47:50 +0200

    fix: clean up

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const { parse } = require("@babel/parser");
 const traverse = require("@babel/traverse").default;
 
-function createAST(source) {
-  return parse(source, {
+function findUsedClasses(file) {
+  const ast = parse(file, {
     sourceType: "module",
     plugins: ["jsx", "typescript"]
   });
-}
 
-function traverseAST(ast, indentifiers = {}) {
-  return traverse(ast, {
-    ...indentifiers
+  const usedClasses = [];
+  let styleImportName;
+
+  traverse(ast, {
+    ImportDeclaration: ({ node: { source, specifiers } }) => {
+      if (source.value.includes(".scss")) {
+        styleImportName = specifiers.find(
+          node => node.type === "ImportDefaultSpecifier"
+        ).local.name;
+      }
+    },
+    MemberExpression: ({ node: { object, property } }) => {
+      if (object.name === styleImportName) {
+        usedClasses.push(property.name);
+      }
+    }
   });
+
+  return usedClasses;
 }
 
 module.exports = {
-  createAST,
-  traverseAST
+  findUsedClasses
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,4 @@
-const { createAST, traverseAST } = require("../index");
+const { findUsedClasses } = require("../index");
 
 const style = `
 .title {
@@ -27,32 +27,8 @@ export const Title: FunctionComponent<Props> = ({ title }) => {
 };
 `;
 
-const ast = createAST(file);
-
-describe("createAST", () => {
-  it("it just should work", () => {
-    expect(ast.type).toEqual("File");
-  });
-});
-
-describe("traverse", () => {
-  it("find used classes", () => {
-    const usedClasses = [];
-    let styleImportName;
-    traverseAST(ast, {
-      ImportDeclaration: ({ node: { source, specifiers } }) => {
-        if (source.value.includes(".scss")) {
-          styleImportName = specifiers.find(
-            node => node.type === "ImportDefaultSpecifier"
-          ).local.name;
-        }
-      },
-      MemberExpression: ({ node: { object, property } }) => {
-        if (object.name === styleImportName) {
-          usedClasses.push(property.name);
-        }
-      }
-    });
-    expect(usedClasses).toEqual(["title"]);
+describe("purge-style", () => {
+  it("should find used classes", () => {
+    expect(findUsedClasses(file)).toEqual(["title"]);
   });
 });
